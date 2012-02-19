4aedfe0d74b2527516de7efa20567abfbca4b3c0
Jim Ivanoff <jim@polarbeardesign.net>
2012-02-19 07:38:16 -0600

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 DdsApp::Application.routes.draw do
+  
+  scope "/~devildog" do
+  
   resources :news
 
   resources :events
@@ -70,4 +73,6 @@ DdsApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+end
 end
