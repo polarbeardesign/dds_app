d04bf339542bbfde67c290d8adc2327e4d1ad1b6
Jim Ivanoff <jim@polarbeardesign.net>
2012-02-26 11:01:48 -0600

    updating events on home page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ActiveRecord::Base
   belongs_to :event_type
+
+scope :published, lambda {
+  where events.start IS NOT NULL AND events.start <= ?", Time.zone.now)
+  }
+
+scope :recent, order("events.start ASC")
+
+
 end
