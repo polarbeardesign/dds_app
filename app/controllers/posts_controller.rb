03ea9102773c3c6dde3b2e151dbd3b3d6834bb83
Jim Ivanoff <jim@polarbeardesign.net>
2012-05-11 11:35:21 -0500

    updated code to allow post edits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,6 +39,7 @@ class PostsController < ApplicationController
   # GET /posts/1/edit
   def edit
     @post = Post.find(params[:id])
+    @user = User.find(session[:current_user])
   end
 
   # POST /posts
