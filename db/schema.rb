fa65221aab48ac8c9ecb5887b8c191c60024d49c
Jim Ivanoff <jim@polarbeardesign.net>
2012-05-18 11:32:14 -0500

    added links to photos on galleries pages, formated photo page

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -152,8 +151,6 @@ ActiveRecord::Schema.define(:version => 20120514210405) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
   create_table "products", :force => true do |t|
     t.integer  "item_no"
