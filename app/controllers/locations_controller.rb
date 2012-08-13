ee410f4f40c1d8032e33b8ef0bc041a88524903b
Jim Ivanoff <jim@polarbeardesign.net>
2012-08-13 12:35:07 -0500

    updated locations controller again

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -44,7 +44,7 @@ class LocationsController < ApplicationController
 
     respond_to do |format|
       if @location.save
-        format.html { redirect_to @location, notice: 'Location was successfully created.' }
+        format.html { redirect_to @location, :notice => 'Location was successfully created.' }
         format.json { render :json => @location, status: :created, location: @location }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class LocationsController < ApplicationController
 
     respond_to do |format|
       if @location.update_attributes(params[:location])
-        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
+        format.html { redirect_to @location, :notice => 'Location was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
