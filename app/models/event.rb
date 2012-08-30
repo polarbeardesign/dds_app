20e4954d3de6c0d454835acae76c3e2894aab48a
Jim Ivanoff <jim@polarbeardesign.net>
2012-08-30 14:06:01 -0500

    updated logic to events published, added f-18 funfact sidebar

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,10 @@ class Event < ActiveRecord::Base
   belongs_to :event_type
   belongs_to :location
 
+
 scope :published, lambda {
 #  where ("events.start IS NOT NULL AND events.start >= ?", Time.zone.now)
-  where ("events.start IS NOT NULL AND events.end > ?"), Time.zone.now
+  where ("events.start IS NOT NULL AND events.end > ?"), (Time.zone.now - 2.day)
   }
 
 scope :ordered, order("events.start ASC")
