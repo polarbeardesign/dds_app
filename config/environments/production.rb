b91c2ae364f1c7cb249449a1f270252e57a37528
Jim Ivanoff <jim@polarbeardesign.net>
2012-12-27 15:47:07 -0600

    updated url for devise production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ DdsApp::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_mailer.default_url_options = { :host => 'www.devildogsquadron.com' }
+  config.action_mailer.default_url_options = { :host => 'devildogsquadron.com' }
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
