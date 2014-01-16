016425f2f23c757baaa319943966262682bf5d41
Jim Ivanoff <jim@polarbeardesign.net>
2014-01-16 16:38:19 -0600

    updates to users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users = User.member_ordered
 
     respond_to do |format|
       format.html # index.html.erb
