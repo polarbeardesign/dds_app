9a3d89500e10e7fb90052ea82dbf174d8da3f024
Jim Ivanoff <jim@polarbeardesign.net>
2014-01-27 14:53:03 -0600

    added datetime picker to events forms

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,6 @@ class Location < ActiveRecord::Base
 
   has_many :events
 
+  scope :ordered, order("locations.short_name ASC")
+
 end
---
+++ db/migrate/20140119070225_create_event_statuses.rb
@@ -0,0 +1,9 @@
+class CreateEventStatuses < ActiveRecord::Migration
+  def change
+    create_table :event_statuses do |t|
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
