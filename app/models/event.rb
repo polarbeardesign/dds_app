9ba3bc5f5f5429828a9588cf7bc72304bb700b6a
Jim Ivanoff <jim@polarbeardesign.net>
2014-03-15 08:47:56 -0500

    fixed hp calendar to include all event types

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,7 +11,8 @@ scope :published, lambda {
   where ("events.start IS NOT NULL AND events.end > ?"), (Time.zone.now - 2.day)
   }
 
-scope :ordered, order("events.event_type_id ASC", "events.start ASC")
+scope :ordered, order("events.start ASC")
+scope :cat_ordered, order("events.event_type_id ASC", "events.start ASC")
 scope :confirmed, where("event_statuses_id = 1")
 scope :tease, :limit => 4
 
