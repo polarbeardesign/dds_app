2a40645427e0a61f40d7aefa8be18a00bbe25b59
Jim Ivanoff <jim@polarbeardesign.net>
2014-04-21 12:50:03 -0500

    formatting updates to locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,7 @@ class LocationsController < ApplicationController
   # GET /locations
   # GET /locations.json
   def index
-    @locations = Location.all
+    @locations = Location.ordered
 
     respond_to do |format|
       format.html # index.html.erb
