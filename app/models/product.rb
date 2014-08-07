137832b5a6d91872cf93630a1aba5f45d1e3bd77
Jim Ivanoff <jim@polarbeardesign.net>
2014-08-07 11:21:44 -0500

    updates to PX sort order

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   has_and_belongs_to_many :sizes
 
   scope :available, where(:available => true)
-  default_scope :order => "field(item_no,'100','500','501','400','401')"
+  default_scope :order => "field(item_no,'100','500','501','600','601','400','401')"
 
 def total_value
   value + ship_handling
