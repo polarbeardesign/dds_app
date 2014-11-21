ba6f4b32137d4705ca50a3085ef7d38fa170ca85
Jim Ivanoff <jim@polarbeardesign.net>
2014-11-21 16:32:31 -0600

    added function to rotate header images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+# http://hivelogic.com/articles/random-image-rotation-in-rails
+  def random_image
+    image_files = %w( .jpg .gif .png )
+    files = Dir.entries(
+      Rails.root.to_s + "/app/assets/images/headers/" 
+    ).delete_if { |x| !image_files.index(x[-4,4]) }
+    files[rand(files.length)]
+  end
+
 end
