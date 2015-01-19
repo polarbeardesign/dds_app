fd54ec393196b8edb70d553a27c30917b8a31246
Jim Ivanoff <jim@polarbeardesign.net>
2015-01-19 11:22:34 -0600

    added validation on creating PX items

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,11 @@ class Product < ActiveRecord::Base
   scope :available, where(:available => true)
   default_scope :order => "field(item_no,'100','500','501','600','601','400','401')"
 
+  validates :name, :item_no, :description, :value, :ship_handling, :sizes, :presence => true, :on => :create
+
+#display_order
+
+
 def total_value
   value + ship_handling
 end
