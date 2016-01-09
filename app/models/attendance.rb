4bdd856228cb3cd4e2ef6e5b9b1f3cabaf68e0e5
Jim Ivanoff <jim@polarbeardesign.net>
2016-01-09 15:03:38 -0600

    added formatting to attendances view

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -10,6 +10,10 @@ class Attendance < ActiveRecord::Base
 
   scope :ordered, order("FIELD(commitment_level, 'Definite','Maybe','Canceled')")
 
+  scope :rsvp_event_ordered, joins(:event).merge(Event.ordered)
+  
+  scope :future, joins(:event).merge(Event.published)
+
   scope :definite, lambda { where ("commitment_level = ?"), ("Definite") }
   scope :maybe, lambda { where ("commitment_level = ?"), ("Maybe") }
   scope :canceled, lambda { where ("commitment_level = ?"), ("Canceled") }
