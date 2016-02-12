0826d64124a0feb19d7e393f9e40e35350fb7e72
Jim Ivanoff <jim@polarbeardesign.net>
2016-02-12 13:19:25 -0600

    fixed typo lfhe to lhfe, used has_lhfe on rides page to show all opportunities not just airshows

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,7 +18,7 @@ skip_before_filter :check_authorization, :check_authentication, :only => [:home,
   end
 
   def living_flight_history_experience
-    @rides = Event.published.ordered.has_lfhe
+    @rides = Event.published.ordered.has_lhfe
     @price = Product.find_by_id(11)
   end
 
