8ab16d64485a6826b4124092f83332a412c26843
Jim Ivanoff <jim@polarbeardesign.net>
2016-02-24 16:15:12 -0600

    adding sort functionality to members view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,5 +26,12 @@ def markdown(text)
   return markdown.render(text).html_safe
 end
 
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
+
 
 end
