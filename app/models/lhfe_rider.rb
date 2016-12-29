fdb679de371701b0434fca7855d283d6df9809f1
Jim Ivanoff <jim@polarbeardesign.net>
2016-12-29 12:26:54 -0600

    added reverse chronological order to lhfe riders

--- app/models/lhfe_rider.rb
+++ app/models/lhfe_rider.rb
@@ -3,4 +3,6 @@ class LhfeRider < ActiveRecord::Base
   attr_accessible :city, :dob, :email, :event_id, :first_name, :gender, :home_phone, :last_name, :married, :occupation, :payment_amount, :payment_type, :ride_date, :spouse_name, :state, :street_1, :street_2, :work_phone, :zip
 
   scope :ordered, order("lhfe_riders.ride_date ASC")
+  scope :reverse_chron, order("lhfe_riders.ride_date DESC")
+  
 end
