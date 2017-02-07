695d8d418923a3c3bf962c7122e769c057b3bcf1
Jim Ivanoff <jim@polarbeardesign.net>
2017-02-07 16:31:29 -0600

    added sponsors model, formatting

--- db/migrate/20170118180528_create_contents.rb
+++ db/migrate/20170118180528_create_contents.rb
@@ -1,14 +1,14 @@
 class CreateContents < ActiveRecord::Migration
-  def change
-    create_table :contents do |t|
-      t.references :user
-      t.string :title
-      t.text :body
-      t.string :slug
-      t.datetime :published_at
-
-      t.timestamps
-    end
-    add_index :contents, :user_id
-  end
+#  def change
+#    create_table :contents do |t|
+#      t.references :user
+#      t.string :title
+#      t.text :body
+#      t.string :slug
+#      t.datetime :published_at
+#
+#      t.timestamps
+#    end
+#    add_index :contents, :user_id
+#  end
 end
