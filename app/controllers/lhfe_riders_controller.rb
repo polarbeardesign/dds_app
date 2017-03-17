7ca40746d4a8a9ce430a8056b3963228e50ebdea
Jim Ivanoff <jim@polarbeardesign.net>
2017-03-17 10:10:56 -0500

    added riders age graph to demographics

--- app/controllers/lhfe_riders_controller.rb
+++ app/controllers/lhfe_riders_controller.rb
@@ -7,6 +7,7 @@ class LhfeRidersController < ApplicationController
     @lhfe_riders = LhfeRider.reverse_chron.all
     @males = LhfeRider.male
     @females = LhfeRider.female
+    @age_brackets = LhfeRider.find_by_sql("SELECT SUM(IF(TIMESTAMPDIFF(year,dob,NOW()) >= 10 AND (TIMESTAMPDIFF(year,dob,NOW()) < 20), 1, 0)) AS `a`, SUM(IF(TIMESTAMPDIFF(year,dob,NOW()) >= 20 AND (TIMESTAMPDIFF(year,dob,NOW()) < 30), 1, 0)) AS `b`, SUM(IF(TIMESTAMPDIFF(year,dob,NOW()) >= 30 AND (TIMESTAMPDIFF(year,dob,NOW()) < 40), 1, 0)) AS `c`, SUM(IF(TIMESTAMPDIFF(year,dob,NOW()) >= 40 AND (TIMESTAMPDIFF(year,dob,NOW()) < 50), 1, 0)) AS `d`, SUM(IF(TIMESTAMPDIFF(year,dob,NOW()) >= 50 AND (TIMESTAMPDIFF(year,dob,NOW()) < 60), 1, 0)) AS `e`, SUM(IF(TIMESTAMPDIFF(year,dob,NOW()) >= 60 AND (TIMESTAMPDIFF(year,dob,NOW()) < 70), 1, 0)) AS `f`, SUM(IF(TIMESTAMPDIFF(year,dob,NOW()) >= 70 AND (TIMESTAMPDIFF(year,dob,NOW()) < 80), 1, 0)) AS `g`, SUM(IF(TIMESTAMPDIFF(year,dob,NOW()) >= 80, 1, 0)) AS `h` FROM lhfe_riders")
     respond_with(@lhfe_riders)
   end
 
