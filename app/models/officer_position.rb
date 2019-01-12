215f8793f24a7f88fb9c2331259a1ac85fc33b75
Jim Ivanoff <jim@polarbeardesign.net>
2019-01-12 16:21:36 -0600

    updated officer custom sort otrder list

--- app/models/officer_position.rb
+++ app/models/officer_position.rb
@@ -4,6 +4,6 @@ class OfficerPosition < ActiveRecord::Base
   has_many :members, :through => :terms
   has_paper_trail
 
-  default_scope :order => "field(position_name,'Unit Leader','Executive Officer','Finance Officer','Operations Officer','Maintenance Officer','Safety Officer','Adjutant')"
+  default_scope :order => "field(position_name,'Unit Leader','Executive Officer','Finance Officer','Operations Officer','Maintenance Officer','Safety Officer','Adjutant','Education Officer','Development Officer')"
 
 end
